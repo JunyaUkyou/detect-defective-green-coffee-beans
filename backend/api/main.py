3c09631db2364f86e664ca4b7ba02505af56d647
ukyogw <ukyogw@gmail.com>
2024-09-25 21:21:45 +0900

    Add: prediction of AutoEncoder

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,7 +5,12 @@ from PIL import Image, ImageDraw
 import io
 import json
 import os
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import transforms
+import numpy as np
 
 
 
@@ -44,11 +49,54 @@ async def ssd(file: UploadFile = File(...)):
 
 
 @app.post('/autoencoder')
-async def autoencoder():
+async def autoencoder(file: UploadFile = File(...)):
+  
+  # 画像を開く
+  image = Image.open(file.file)
+  
+  # 現在のディレクトリ取得
   current_dir = os.path.dirname(os.path.abspath(__file__))
-  json_path = os.path.join(current_dir, 'threshold.json')
+  
+  # 学習済みモデル、閾値取得
+  model_path = os.path.join(current_dir, 'Autoencoder_back.pt')
+  threshold_path = os.path.join(current_dir, 'threshold.json')
 
-  with open(json_path, 'r') as f:
+  # 学習済みモデルロード
+  model = torch.jit.load(model_path)  
+  
+  # 前処理
+  transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+  ])
+  
+  # 推論
+  model.eval()
+  x = transform(image)
+  x = x.unsqueeze(0)
+  #print('x', x)
+  #print('x shape', x.shape)
+  y = model(x)
+  #print('y', y)
+  
+  # 平均二乗誤差
+  mse_loss = F.mse_loss(y, x, reduction='none')
+  # 損失合計を算出
+  total_loss = np.sum(mse_loss.detach().numpy(), axis=(1, 2, 3))
+  
+    # 欠点豆判定
+  with open(threshold_path, 'r') as f:
     config = json.load(f)
     threshold = float(config["autoencoder"])
-  return threshold
+    print('total_loss', total_loss)
+    print('total_loss_0', total_loss[0])
+    print('threshold', threshold)
+    
+    print('判定', threshold < total_loss[0])
+    
+    # 閾値より損失が大きい場合は欠点豆と判断する
+    if (threshold < total_loss[0]):
+      result = '欠点豆'
+    else:
+      result = '良質豆'
+  return result
