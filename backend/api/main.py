47b0c9e85d6bdaf20a22e6152f6419c9ce1c8e7e
ukyogw <ukyogw@gmail.com>
2024-09-25 18:26:07 +0900

    Definition: autoencoder function

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,21 +3,26 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from PIL import Image, ImageDraw
 import io
+import json
+import os
+
+
+
 
 app = FastAPI()
 
 # 許可するURL
 origins = [
-    "http://localhost:5173", # フロントエンドのURL
+  "http://localhost:5173", # フロントエンドのURL
 ]
 
 # CORS 設定を追加
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,  
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 @app.post('/ssd')
@@ -36,3 +41,14 @@ async def ssd(file: UploadFile = File(...)):
   img_io.seek(0)
 
   return StreamingResponse(img_io, media_type="image/jpeg")
+
+
+@app.post('/autoencoder')
+async def autoencoder():
+  current_dir = os.path.dirname(os.path.abspath(__file__))
+  json_path = os.path.join(current_dir, 'threshold.json')
+
+  with open(json_path, 'r') as f:
+    config = json.load(f)
+    threshold = float(config["autoencoder"])
+  return threshold
