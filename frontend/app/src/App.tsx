6e1d177f1319b363cbfa9b9f194859a3d717a380
ukyogw <ukyogw@gmail.com>
2024-10-05 06:29:47 +0900

    デザイン

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,67 +1,56 @@
 import React, { useState } from 'react';
 import './App.css';
+import UploadImage from './UploadImage';
+import good1 from './assets/2EB011BF-CAA1-4470-9376-2E42A8B87CDE_1_105_c.jpeg';
+import good2 from './assets/4A0389F9-050D-4FF2-9230-1B434ACB44DE_4_5005_c.jpeg';
+import bad1 from './assets/bad1.jpg';
+import bad2 from './assets/bad2.jpeg';
 
 const App = () => {
-  //const [file, setFile] = useState<File | null>(null);
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-
-  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log({ e });
-    const files = e.target.files;
-    if (files && files[0]) {
-      //setFile(files[0]);
-      onClickSubmit(files[0]);
-    }
-  };
-
-  const onClickSubmit = (file: File) => {
-    if (!file) {
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append('file', file);
-
-    fetch('http://localhost:8000/ssd', {
-      method: 'POST',
-      body: formData, // ファイルを含めたFormDataを送信
-    })
-      .then((response) => response.blob()) // Blob 形式で画像を取得
-      .then((blob) => {
-        const url = URL.createObjectURL(blob);
-        console.log({ url });
-        setImageUrl(url); // 画像を表示するためにURLを生成
-      })
-      .catch((error) => {
-        console.error('リクエストエラー:', error);
-      });
-  };
+  const images = [
+    {
+      src: good1,
+      description: '良質豆 1個',
+    },
+    {
+      src: good2,
+      description: '良質豆 3個',
+    },
+    {
+      src: bad1,
+      description: '欠点豆 1個',
+    },
+    {
+      src: bad2,
+      description: '欠点豆 2個',
+    },
+  ];
 
   return (
     <>
       <div className="upload-container">
         <div className="fileUpload">
-          <p className="aaa">
-            コーヒーの欠点豆の画像をアップロードしてください
-            <br className="br"></br>
-            （※画像は幅と高さを同じにしてください）
-          </p>
-
-          <label className="bbb" htmlFor="sample1">
-            ファイルを選択
-            <input
-              type="file"
-              id="sample1"
-              accept="image/*"
-              onChange={onChangeFile}
-            />
-          </label>
+          <img src="/public/images/4307_color.png" className="parson" />
+          <div className="top-center">
+            <p className="aaa">
+              コーヒー豆の<span className="emphasis">欠点豆検出</span>
+              のデモサイトです
+            </p>
+            <div className="coffee">
+              <img src="/public/images/1421_color.png" className="coffee-cap" />
+            </div>
+          </div>
+          <img src="/public/images/4304_color.png" className="parson" />
         </div>
 
-        <div className="upload-result">
-          <p>アップロード済み</p>
-          {imageUrl && <img src={imageUrl} alt="Downloaded" />}
-        </div>
+        {images.map((image, index) => (
+          <UploadImage
+            key={index}
+            src={image.src}
+            description={image.description}
+            imageNumber={index + 1}
+          />
+        ))}
       </div>
     </>
   );
---
+++ frontend/app/src/UploadImage.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+
+interface UploadImageProps {
+  src: string;
+  description: string;
+  imageNumber: number;
+}
+
+const UploadImage: React.FC<UploadImageProps> = ({
+  src,
+  description,
+  imageNumber,
+}) => {
+  const getFileName = (path: string) =>
+    path.substring(path.lastIndexOf('/') + 1);
+
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const paddedImageNumber = String(imageNumber).padStart(2, '0');
+
+  const onClickSubmit = async () => {
+    setIsLoading(true);
+    // 画像を src からフェッチして Blob に変換
+    const response = await fetch(src);
+    const blob = await response.blob();
+
+    // Blob から File オブジェクトを作成
+    const fileName = getFileName(src);
+    const file = new File([blob], fileName, { type: blob.type });
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('http://localhost:8000/ssd', {
+      method: 'POST',
+      body: formData, // ファイルを含めたFormDataを送信
+    })
+      .then((response) => response.blob()) // Blob 形式で画像を取得
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        setIsLoading(false);
+        console.log({ url });
+        setImageUrl(url); // 画像を表示するためにURLを生成
+      })
+      .catch((error) => {
+        console.error('リクエストエラー:', error);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <div className="upload-image">
+        <div className="description">
+          <h3 className="heading" data-number={paddedImageNumber}>
+            <span>{description}</span>
+          </h3>
+        </div>
+        <div className="upload-result">
+          <div className="upload-left">
+            <img src={src} alt="Uploaded" />
+            <button onClick={onClickSubmit} />
+            <a className="upload-button" href="">
+              推論する
+            </a>
+          </div>
+
+          <div className="upload-right">
+            {isLoading ? (
+              <img
+                src="/public/images/loading.gif"
+                className="upload-loading"
+              />
+            ) : (
+              <img
+                src={imageUrl ? imageUrl : '/public/images/no-image.png'}
+                className={imageUrl ? 'downloaded' : 'no-image'}
+                alt="Downloaded"
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UploadImage;
