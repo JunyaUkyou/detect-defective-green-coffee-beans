e68a9c8d989fd03e83bf9ca7499299d5cff9da0c
ukyogw <ukyogw@gmail.com>
2024-11-02 16:27:34 +0900

    main.py autopep8適用

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,26 +5,26 @@ from fastapi.middleware.cors import CORSMiddleware
 # 別ファイルからインポート
 from .events import startup          # イベント
 from .routers import ssd             # ルーティング
-from core.config import FRONTEND_URL # 設定値
+from core.config import FRONTEND_URL  # 設定値
 
 app = FastAPI()
 
 # 許可するURL
 origins = [
-  FRONTEND_URL, # フロントエンドのURL
+    FRONTEND_URL,  # フロントエンドのURL
 ]
 
 # CORS 設定を追加
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # イベントの登録
-app.add_event_handler("startup", startup.load_ssd) # 起動時に実行する
+app.add_event_handler("startup", startup.load_ssd)  # 起動時に実行する
 
 # ルーターの登録
 app.include_router(ssd.router)
