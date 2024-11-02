b12ac8f6001fecd9339937d130b5b4566ef9b908
ukyogw <ukyogw@gmail.com>
2024-11-02 15:56:13 +0900

    load_ssd コメント

--- backend/api/events/startup.py
+++ backend/api/events/startup.py
@@ -8,9 +8,17 @@ from core.config import FRONTEND_URL   # 設定値
 logger = getLogger("uvicorn.app")
 
 async def load_ssd():
+    """
+    SSDモデルをアプリ起動時に読み込む
+
+    この関数はアプリの起動イベントで呼ばれ、SSDモデルを読み込みます。
+    起動イベントでSSDモデルを読み込む理由は、
+    事前学習済みモデルの重みファイルがキャッシュに存在しない場合は
+    重みファイルのダウンロード処理が行われます。
+    このダウンロード処理に1分程度はかかるため、
+    アプリ起動時にダウンロード処理を行うことで、
+    推論時のAPIリクエストの応答時間を短縮します。
+    """
     logger.info("startup event load_ssd start")
-    # アプリ起動時にモデルを読み込む
-    # 推論時に事前学習済みモデルの重みファイルがキャッシュに存在しない場合ダウンロード処理が行われる
-    # 推論時でダウンロード処理が加わるとAPIレスポンスが遅くなるためアプリ起動時にダウンロードを行う
     ssd_predictor.load_model()
     logger.info("startup event load_ssd end")
