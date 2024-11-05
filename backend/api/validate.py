4267f22ee0d605a8387ccf63697d7e3c297267b0
ukyogw <ukyogw@gmail.com>
2024-11-05 13:47:48 +0900

    backend バリデーション

--- backend/api/validate.py
+++ backend/api/validate.py
@@ -1,6 +1,7 @@
 from fastapi import File, HTTPException, UploadFile
 from PIL import Image
 
+
 async def validate_image(file: UploadFile = File(...)):
     # ファイル形式の確認
     if not file.content_type.startswith("image/"):
@@ -10,10 +11,11 @@ async def validate_image(file: UploadFile = File(...)):
     if file.size > 1024 * 1024:  # 例: 1MB以下
         raise HTTPException(status_code=400, detail="ファイルサイズが大きすぎます")
 
-    # 画像サイズの確認
+    # アップロード画像取得
     image = Image.open(file.file)
-    if image.width != image.height:
-      raise HTTPException(status_code=400, detail="画像の幅と高さは一致している必要があります")
 
+    # 画像が正方形か確認
+    if image.width != image.height:
+        raise HTTPException(status_code=400, detail="画像の幅と高さは一致している必要があります")
 
     return file
